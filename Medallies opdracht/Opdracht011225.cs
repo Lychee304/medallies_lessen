bcfb312302085cefeb89558f8bb7e9011172fd73
Lychee304 <2173013@talnet.nl>
2025-12-01 14:53:07 +0100

    Opdracht 8 backup
    save for later, its almost complete

--- Medallies opdracht/Opdracht011225.cs
+++ Medallies opdracht/Opdracht011225.cs
@@ -1,6 +1,4 @@
-﻿/*
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,77 +9,21 @@ namespace Medallies_opdracht
 {
     internal class Opdracht011225
     {
-        private string _fName;
-        private string _lName;
-        private int _age;
+        private string _name;
+        private int _level;
+        private int _lives;
 
-        public Opdracht011225(string fName, string lName, int age)
+        public Opdracht011225(string name, int level, int lives)
         {
-            _fName = fName;
-            _lName = lName;
-            _age = age;
-            Console.WriteLine("hoi " + fName + " " + lName + ", your " + age + " years old, welcome bro :)");
+            _name = name;
+            _level = level;
+            _lives = lives;
 
         }
-
         
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Medallies_opdracht
-{
-    internal class Program
-    {
-
-        private static string _fName = "amir";
-        private static string _lName = "allouti";
-        private static int _age = 19;
-
-        static void Main(string[] args)
+        public string ShowStats()
         {
-
-            Opdracht011225 student = new Opdracht011225(_fName, _lName, _age);
-            Opdracht011225 student2 = new Opdracht011225("first", "last", 69);
-
-            
-            Opdracht011225 opdracht = new Opdracht011225();
-            opdracht.Class();
-            
-            
-            Console.WriteLine("Hoe heet je?");
-            string input = Console.ReadLine();
-            Console.WriteLine("en hoe oud ben je?");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Hoi " + input + ", je bent " + input1 + " jaar oud!");
-            
+            return "hi, i am " + _name + ", i have " + _level + " health and " + _lives + " lives!";
         }
     }
 }
-
-*/
--- Medallies opdracht/Program.cs
+++ Medallies opdracht/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,33 @@ namespace Medallies_opdracht
     internal class Program
     {
 
+        private static string _playerName = "amir";
         static void Main(string[] args)
         {
 
-            Opdracht7 opdracht = new Opdracht7();
-            opdracht.Cube();
+
+                Opdracht011225 player = new Opdracht011225(_playerName, 40, 4);
+                Console.WriteLine(player.ShowStats());
+
+                Opdracht011225 player2 = new Opdracht011225("a stray cat", 30, 9);
+                Console.WriteLine(player2.ShowStats());
+
+                Opdracht011225 player3 = new Opdracht011225("class construction ltd", 420, 69);
+                Console.WriteLine(player3.ShowStats());
+
+
+            List<string, int> playBase = new List<string, int>()
+            {
+                player,
+                player2,
+                player3
+            };
 
             /*
+            Opdracht011225 opdracht = new Opdracht011225();
+            opdracht.Class();
+            
+            
             Console.WriteLine("Hoe heet je?");
             string input = Console.ReadLine();
             Console.WriteLine("en hoe oud ben je?");
